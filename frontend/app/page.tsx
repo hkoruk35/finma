9ca1a6ce5ec9165cbbfdf27a3f08407eada17d00
hkoruk35 <development@globalledger.com>
2026-03-30 07:22:46 -0400

    Revert "fix: Remove sidebar from landing page"
    This reverts commit ea20946d88982f8c222cdf478d5055f92153df4c.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1505,7 +1505,7 @@ export default function LandingPage() {
     <div className="lp-root" dir={isRtl ? 'rtl' : 'ltr'} onClick={() => langOpen && setLangOpen(false)}>
       {/* Close lang dropdown on outside click is handled above */}
 
-      {/* Sidebar not shown on landing page */}
+      <Sidebar />
       {showInstall && <InstallBanner />}
       <Header />
 
