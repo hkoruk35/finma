9104ee6418fba635ce4122036dea7506c854e6d7
hkoruk35 <development@globalledger.com>
2026-06-18 10:00:50 -0400

    Data: Terminal Pulse 10:00

--- inday313.py
+++ inday313.py
@@ -1534,6 +1534,7 @@ async def send_telegram_message(message: str) -> None:
 async def send_telegram_photo(photo_path: str, caption: str = "") -> None:
     return  # 🚫 TELEGRAM MESAJ GÖNDERİMİ PASİF HALE GETİRİLDİ
     if not ENABLE_TELEGRAM_NOTIFICATIONS:
+        return
     if not os.path.exists(photo_path):
         return
 
