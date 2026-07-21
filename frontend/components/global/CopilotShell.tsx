711609e0b66948f3f315c171624c33255a702f4b
hkoruk35 <hkoruk35@gmail.com>
2026-07-21 16:30:56 -0400

    Remove MarketMoodBar ticker

--- frontend/components/global/CopilotShell.tsx
+++ frontend/components/global/CopilotShell.tsx
@@ -3,12 +3,10 @@
 import React, { ReactNode } from "react";
 import { CopilotProvider } from "@/context/CopilotContext";
 import CopilotDrawer from "./CopilotDrawer";
-import MarketMoodBar from "./MarketMoodBar";
 
 export default function CopilotShell({ children }: { children: ReactNode }) {
   return (
     <CopilotProvider>
-      <MarketMoodBar />
       {children}
       <CopilotDrawer />
     </CopilotProvider>
